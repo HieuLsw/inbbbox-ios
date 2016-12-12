d7cb8a739a86ed7fd3366ca1b1cad8d1894cd46c
Błażej Wdowikowski <blazej@wdowikowski.pl>
2016-12-12 16:35:19 +0100

    Fixed tests

--- Inbbbox/Source Files/ViewModels/FolloweesViewModel.swift
+++ Inbbbox/Source Files/ViewModels/FolloweesViewModel.swift
@@ -55,7 +55,7 @@ class FolloweesViewModel: BaseCollectionViewViewModel {
         
         downloadShots(for: followee)
     }
-    
+
     func followeeCollectionViewCellViewData(_ indexPath: IndexPath) -> FolloweeCollectionViewCellViewData {
         return FolloweeCollectionViewCellViewData(followee: followees[indexPath.row],
                 shots: followeesIndexedShots[indexPath.row])
