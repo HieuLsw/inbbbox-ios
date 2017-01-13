93bfc863855438d066d40b917971724379a0e53a
Marcin Siemaszko <siemman@gmail.com>
2017-01-13 14:43:35 +0100

    add shots state handlers provider spec

---
+++ Unit Tests/ShotsStateHandlersProviderSpec.swift
@@ -0,0 +1,41 @@
+//
+//  ShotsStateHandlersProviderSpec.swift
+//  Inbbbox
+//
+//  Copyright © 2017 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import Inbbbox
+
+class ShotsStateHandlersProviderSpec: QuickSpec {
+    
+    override func spec() {
+        
+        var sut: ShotsStateHandlersProvider!
+        
+        beforeEach {
+            sut = ShotsStateHandlersProvider()
+        }
+        
+        afterEach {
+            sut = nil
+        }
+        
+        describe("handler for state") {
+            context("should return proper handler for state") {
+                it("types of values should be proper") {
+                expect(sut.shotsStateHandlerForState(.initialAnimations) is ShotsInitialAnimationsStateHandler).to(beTruthy())
+                expect(sut.shotsStateHandlerForState(.onboarding) is ShotsOnboardingStateHandler).to(beTruthy())
+                expect(sut.shotsStateHandlerForState(.normal) is ShotsNormalStateHandler).to(beTruthy())
+                }
+                
+            }
+        }
+        
+    }
+}
+
+
