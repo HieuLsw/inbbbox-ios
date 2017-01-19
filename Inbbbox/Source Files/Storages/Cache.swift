814183a3f6b5a41b0aafec9a9a00cf70b9f298fa
Lukasz Pikor <vonpik@gmail.com>
2017-01-19 15:39:27 +0100

    Renaming elements.

--- Inbbbox/Source Files/Storages/Cache.swift
+++ Inbbbox/Source Files/Storages/Cache.swift
@@ -18,39 +18,39 @@ struct SharedCache {
 
 final class Cache<T> where T:Hashable, T:Sortable {
 
-    private var elements2 = Set<T>()
+    private var elements = Set<T>()
 
     /// Returns number of items.
     var count: Int {
-        return elements2.count
+        return elements.count
     }
 
     /// Appends element to cache. Replaces duplicate, if any.
     /// - parameter element: Element to append.
     func add(_ element: T) {
-        elements2.update(with: element)
+        elements.update(with: element)
     }
 
     /// Adds contents of array to cache. Replaces duplicates, if any.
     /// - parameter elements: Array to add.
     func add(_ elements: [T]) {
-        elements.forEach { elements2.update(with: $0) }
+        elements.forEach { elements.update(with: $0) }
     }
 
     /// Returns all elements, sorted by `createdAt`, descending.
     /// - returns: All elements.
     func all() -> [T] {
-        return elements2.sorted { $0.createdAt.compare($1.createdAt as Date) == .orderedDescending }
+        return elements.sorted { $0.createdAt.compare($1.createdAt as Date) == .orderedDescending }
     }
 
     /// Removes single element
     /// - parameter element: Element to remove.
     func remove(_ element: T) {
-        elements2.remove(element)
+        elements.remove(element)
     }
 
     /// Removes all elements
     func removeAll() {
-        elements2.removeAll()
+        elements.removeAll()
     }
 }
