5bc7536aded9d28a4cdcca1bbd59cbcd2ab0662a
Krzysztof Kapitan <me@example.com>
2017-03-16 17:39:35 +0100

    Refactor ShotBucketsViewModelSpec

--- Unit Tests/ShotBucketsViewModelSpec.swift
+++ Unit Tests/ShotBucketsViewModelSpec.swift
@@ -110,123 +110,39 @@ class ShotBucketsViewModelSpec: QuickSpec {
                     expect(sut.buckets.count).to(equal(0))
                 }
                 
-                it("should have correct number of items") {
-                    waitUntil { done in
-                        sut.loadBuckets().then { _ in
-                            done()
-                        }.catch { _ in }
-                    }
-
-                    expect(sut.itemsCount >= 2).to(beTruthy())
-                }
             }
             
             describe("when buckets are loaded") {
                 
-                var didReceiveResponse: Bool?
-                
-                beforeEach {
-                    didReceiveResponse = false
-                    waitUntil { done in
-                        sut.loadBuckets().then { result -> Void in
-                            didReceiveResponse = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                }
-                
-                afterEach {
-                    didReceiveResponse = nil
-                }
-                
-                it("buckets should be properly downloaded") {
-                    expect(didReceiveResponse).to(beTruthy())
-                    expect(didReceiveResponse).toNot(beNil())
-                }
-                
-                it("view model should have correct number of buckets") {
-                    expect(sut.buckets.count).to(equal(2))
-                }
-                
-                it("view model should have correct number of items") {
-                    expect(sut.itemsCount).to(equal(4))
+                it("buckets should be properly downloaded and stored") {
+                    let promise = sut.loadBuckets()
+                    
+                    expect(promise).to(resolveWithValueMatching { _ in
+                        expect(sut.buckets).to(haveCount(2))
+                        expect(sut.itemsCount).to(equal(4))
+                    })
                 }
             }
             
             describe("when adding shot to bucket") {
                 
-                var didReceiveResponse: Bool?
-                
-                beforeEach {
-                    didReceiveResponse = false
-                    
-                    waitUntil { done in
-                        sut.loadBuckets().then { result -> Void in
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                    
-                    waitUntil { done in
-                        sut.addShotToBucketAtIndex(0).then { result -> Void in
-                            didReceiveResponse = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                }
-                
-                afterEach {
-                    didReceiveResponse = nil
-                }
-                
                 it("should be correctly added") {
-                    expect(didReceiveResponse).to(beTruthy())
-                    expect(didReceiveResponse).toNot(beNil())
+                    let promise = sut.loadBuckets().then { sut.addShotToBucketAtIndex(0) }
+                    
+                    expect(promise).to(resolveWithSuccess())
                 }
             }
             
             describe("adding shot to new bucket") {
-                
-                var didReceiveResponse: Bool?
-                var didCreateBucket: Bool?
-                
-                beforeEach {
-                    didReceiveResponse = false
-                    didCreateBucket = false
-                    
-                    waitUntil { done in
-                        sut.loadBuckets().then { result -> Void in
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                    
-                    waitUntil { done in
-                        sut.createBucket("fixture.name").then { result -> Void in
-                            didCreateBucket = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                    
-                    waitUntil { done in
-                        sut.addShotToBucketAtIndex(2).then { result -> Void in
-                            didReceiveResponse = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                }
-                
-                afterEach {
-                    didReceiveResponse = nil
-                    didCreateBucket = nil
-                }
-                
+
                 it("should create new bucket properly") {
-                    expect(didCreateBucket).to(beTruthy())
-                    expect(didCreateBucket).toNot(beNil())
-                }
-                
-                it("should be correctly added") {
-                    expect(didReceiveResponse).to(beTruthy())
-                    expect(didReceiveResponse).toNot(beNil())
+                    let promise = sut.loadBuckets().then {
+                        sut.createBucket("fixture.name")
+                    }.then {
+                        sut.addShotToBucketAtIndex(2)
+                    }
+                    
+                    expect(promise).to(resolveWithSuccess())
                 }
             }
         }
@@ -254,80 +170,31 @@ class ShotBucketsViewModelSpec: QuickSpec {
                     expect(sut.buckets.count).to(equal(0))
                 }
                 
-                it("should have correct number of items") {
-                    waitUntil { done in
-                        sut.loadBuckets().then { _ in
-                            done()
-                        }.catch { _ in }
-                    }
-
-                    expect(sut.itemsCount >= 2).to(beTruthy())
-                }
             }
             
             describe("when buckets for shot are loaded") {
                 
-                var didReceiveResponse: Bool?
-                
-                beforeEach {
-                    didReceiveResponse = false
-                    waitUntil { done in
-                        sut.loadBuckets().then { result -> Void in
-                            didReceiveResponse = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                }
-                
-                afterEach {
-                    didReceiveResponse = nil
-                }
-                
-                it("buckets should be properly downloaded") {
-                    expect(didReceiveResponse).to(beTruthy())
-                    expect(didReceiveResponse).toNot(beNil())
-                }
-                
-                it("view model should have correct number of buckets") {
-                    expect(sut.buckets.count).to(equal(3))
-                }
-                
-                it("view model should have correct number of items") {
-                    expect(sut.itemsCount).to(equal(5))
+                it("buckets should be properly downloaded and stored") {
+                    let promise = sut.loadBuckets()
+                    
+                    expect(promise).to(resolveWithValueMatching { _ in
+                        expect(sut.buckets).to(haveCount(3))
+                        expect(sut.itemsCount).to(equal(5))
+                    })
                 }
             }
             
             describe("when removing shot from selected buckets") {
                 
-                var didReceiveResponse: Bool?
-                
-                beforeEach {
-                    didReceiveResponse = false
-                    
-                    waitUntil { done in
-                        sut.loadBuckets().then { result -> Void in
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                    
-                    _ = sut.selectBucketAtIndex(0)
-                    _ = sut.selectBucketAtIndex(1)
-                    
-                    waitUntil { done in
-                        sut.removeShotFromSelectedBuckets().then { result -> Void in
-                            didReceiveResponse = true
-                            done()
-                        }.catch { _ in fail("This should not be invoked") }
-                    }
-                }
-                
-                afterEach {
-                    didReceiveResponse = nil
-                }
-                
                 it("shot should be properly removed from selected buckets") {
-                    expect(didReceiveResponse).to(beTruthy())
-                    expect(didReceiveResponse).toNot(beNil())
+                    let promise = sut.loadBuckets().then { _ -> Void in
+                        _ = sut.selectBucketAtIndex(0)
+                        _ = sut.selectBucketAtIndex(1)
+                    }.then {
+                        sut.removeShotFromSelectedBuckets()
+                    }
+                    
+                    expect(promise).to(resolveWithSuccess())
                 }
             }
         }
